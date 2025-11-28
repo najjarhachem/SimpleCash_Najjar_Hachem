cc6175f3bf39f7495680daf889bf7c8a3c638750
Hachem <hechemnajjar@gmail.com>
2025-11-28 09:53:11 +0100

    Initial commit

---
+++ src/main/java/org/formation/simplecash_najjar_hachem/SimpleCashNajjarHachemApplication.java
@@ -0,0 +1,13 @@
+package org.formation.simplecash_najjar_hachem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCashNajjarHachemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCashNajjarHachemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/formation/simplecash_najjar_hachem/SimpleCashNajjarHachemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.formation.simplecash_najjar_hachem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCashNajjarHachemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
